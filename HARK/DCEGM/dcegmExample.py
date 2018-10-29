2b8c80886b173306424f364a2ac1d458d3403857
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2018-10-29 11:02:03 +0100

    Initial commit

---
+++ HARK/DCEGM/dcegmExample.py
@@ -0,0 +1,25 @@
+import numpy
+
+from HARK.utilities import CRRAutility, CRRAutility_inv, CRRAutilityP, CRRAutilityP_inv
+from HARK import Solution, AgentType
+
+class DCEGMSolution(Solution):
+    def __init__():
+
+        return None
+
+def dcegmUpperEnvelope(M, C, V):
+
+    return None
+
+def dcegmEGM(M, C, V):
+
+    return None
+
+def DCEGM(M, C, V):
+
+    return None
+
+
+def RetiringDeaton(AgentType):
+    def __init__(self):
