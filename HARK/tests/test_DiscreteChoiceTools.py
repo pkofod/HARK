89977dc06c6c65dfe9c75c47b2083206905a004f
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2018-11-05 20:59:38 +0100

    Add discreteEnvelope.

---
+++ HARK/tests/test_DiscreteChoiceTools.py
@@ -0,0 +1,32 @@
+"""
+This file implements unit tests to check HARK/utilities.py
+"""
+from __future__ import print_function, division
+from __future__ import absolute_import
+
+from builtins import str
+from builtins import zip
+from builtins import range
+from builtins import object
+
+# Bring in modules we need
+import unittest
+import numpy as np
+from HARK.interpolation import discreteEnvelope
+
+class testsForDiscreteChoiceTools(unittest.TestCase):
+
+    #def setUp(self):
+    #    return
+
+    def test_NoTasteShocks(self):
+
+        Vs = np.array([[[0, 1], [9, 0]], [[1,2], [1, 3]]])
+        sigma = 0.0
+
+        Vref = np.array([[1, 2], [9, 3]])
+        Pref = np.array([[[0, 0], [1, 0]], [[1, 1], [0, 1]]])
+        P, V = discreteEnvelope(Vs, sigma)
+
+        self.assertEqual((Vref == V).all(), True)
+        self.assertEqual((Pref == P).all(), True)
